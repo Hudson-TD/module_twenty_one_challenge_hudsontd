4ebf8f7b57b855ddb4798697602da1a929bfd55d
Tyler Hudson <tylerhudson96@gmail.com>
2022-12-04 20:57:31 -0500

    Updating frontend App file to support Apollo and auth middleware with JWT

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,34 @@ import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
-// importing Apollo provider
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+// import ApolloProvider
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-const client = new ApolloClient({
+// GraphQL API endpoint
+const httpLink = createHttpLink({
   uri: "/graphql",
+});
+
+// Middleware - attached JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Exectute auth middleware prior to making the request to our GraphQL API
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
